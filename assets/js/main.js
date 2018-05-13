31d2b05f775bd12dd068f6a787d26caf407c68cb
DuyDo <duydo.dev@gmail.com>
2018-05-13 17:08:56 -0500

    ..

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+console.log("From main.js");
+let imgArray = ["batman.png", "blackpanther.png", "captain.png", "hulk.png", "ironman.png", "spiderman.png", "superman.png", "thor.png"];
+imgArray = imgArray.concat(imgArray);
+//sort array randomlly
+imgArray.sort(function(a, b) {return 0.5 - Math.random()});
+let firstMove = false, secondMove = false;
+
+//function to map imgArray with card
+
+function imgMap(id) {
+    let imgName;
+    switch(id) {
+        case "11":imgName = imgArray[0];break;
+        case "12":imgName = imgArray[1];break;
+        case "13":imgName = imgArray[2];break;
+        case "14":imgName = imgArray[3];break;
+        case "21":imgName = imgArray[4];break;
+        case "22":imgName = imgArray[5];break;
+        case "23":imgName = imgArray[6];break;
+        case "24":imgName = imgArray[7];break;
+        case "31":imgName = imgArray[8];break;
+        case "32":imgName = imgArray[9];break;
+        case "33":imgName = imgArray[10];break;
+        case "34":imgName = imgArray[11];break;
+        case "41":imgName = imgArray[12];break;
+        case "42":imgName = imgArray[13];break;
+        case "43":imgName = imgArray[14];break;
+        case "44":imgName = imgArray[15];break;
+    }
+    return imgName;
+}
+
+//start game , create cards grid 4 x 4
+function start() {
+    let grid = "";
+    for(let i = 0; i < 4; i++){
+        grid += "<tr>";
+        for(let j = 0; j < 4; j++) {
+            grid += "<td " + "id='" + (i+1) + (j+1) + "' ";
+            grid += "class='card' onclick='handleClick(this)' style='background = url('assets/img/question.png')' ></td>"
+        }
+        grid += "</tr>"
+    }
+    const grids = document.getElementById("grid");
+    grids.innerHTML = grid;
+}
+
+//change background img when click
+function handleClick(card) {
+    if (firstMove && secondMove === false) {
+        card.style.background = 'url("assets/img/' + imgMap(card.id) + '")';
+        secondMove = card;
+        setTimeout(function(){checkWin(firstMove, secondMove)}, 500);
+    } else {
+        card.style.background = 'url("assets/img/' + imgMap(card.id) + '")';
+        firstMove = card;
+        console.log(firstMove);
+    }
+}
+
+//check match
+function checkWin(card1, card2) {
+    if(card1.style.background === card2.style.background) {
+        firstMove = false;
+        secondMove = false;
+        card1.onclick = false;
+        card2.onclick = false;
+        
+    } else {
+        firstMove = false;
+        secondMove = false;
+        card1.style.background = "url('assets/img/question.png')";
+        card2.style.background = "url('assets/img/question.png')";
+    }
+}
+
+// reset game
+const reset = document.getElementById("reset");
+reset.addEventListener("click", function(){start()});
+
+start();
